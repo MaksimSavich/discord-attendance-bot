2fc8a601560de4dd2fca28e78fc6f1ff5b05d547
Maksim Savich <savichm2@msu.edu>
2023-10-18 00:27:05 -0400

    0.1.0

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = discord.Client(command_prefix='!',intents=intents)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.command()
+async def test(ctx):
+    await ctx.send('Hello!')
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
